68da36e74e38bac305ea4616cd7c88daeba708f2
Andrei <kisliuk.andrei@gmail.com>
2023-12-12 22:33:54 +0100

    fea: add button component

---
+++ components/Buttons/Button.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+
+interface ButtonProps {
+    title: string,
+    isArrow: boolean,
+    className: string
+}
+export default function Button(
+    {title, isArrow, className} : ButtonProps
+    ) {
+    return <button className={`${className}
+        uppercase
+        cursor-pointer
+        border-solid
+        border-2
+        border-main-red
+        rounded-md`}>
+        {title} {isArrow && (<Image
+            className="ml-5"
+            src="/arrow.png"
+            alt="arrow"
+            width={100}
+            height={2} />)}
+    </button>
+}
