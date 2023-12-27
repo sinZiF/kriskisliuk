263468472572b17f5432c1fcd9c125f9efc050ce
Andrei <kisliuk.andrei@gmail.com>
2023-12-27 15:10:49 +0100

    fix: fixing folders componets

--- components/services/Services.tsx
+++ components/services/Services.tsx
@@ -2,7 +2,7 @@ import LocalFontLayout from "@/app/ui/local-fonts/localFontLayout";
 import LocalFontTitleLayout from "@/app/ui/local-fonts/localFontTitleLayout";
 import Title from "@/app/ui/title/Title";
 import Image from "next/image";
-import Button from "../Buttons/Button";
+import Button from "../buttons/Button";
 import { Fragment } from "react";
 
 
